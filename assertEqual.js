e56d800ba246c8bc583fdc2a1979095266eb77e0
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-07 20:50:04 +0000

    tail

--- assertEqual.js
+++ assertEqual.js
@@ -2,19 +2,10 @@
 const assertEqual = function(actual, expected) {
   let result = "";
   if (actual === expected) {
-    // result += "✅✅✅ Assertion Passed: ";
-    // result += actual;
-    // result += " === ";
-    // result += expected;
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    // result += "🛑🛑🛑 Assertion Failed: ";
-    // result += actual;
-    // result += " !== ";
-    // result += expected;
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-  // console.log(result);
 };
 
 // TEST CODE
