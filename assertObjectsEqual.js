cea1ffa24de6503ed7fd31f1c11a03f7b9803671
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-08 22:57:14 +0000

    assert objects equal updated inspect

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -27,53 +27,54 @@ const eqObjects = function(ob1, ob2) {
 };
 
 const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; // <= add this line
   if (eqObjects(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
-// Test Cases
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertObjectsEqual(ab, ba);   //true
+// // Test Cases
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertObjectsEqual(ab, ba);   //true
 
-const abc = { a: "1", b: "2", c: "3" };
-assertObjectsEqual(ab, abc);  //false
+// const abc = { a: "1", b: "2", c: "3" };
+// assertObjectsEqual(ab, abc);  //false
 
-const media1 = {
-  game: { 
-    title: "Final Fantasy IX",
-    composer: "Nobuo Uematsu"
-  }, 
-  movie: {
-    title: "Lord of the Rings",
-    composer: "Howard Shore"
-  }
-};
+// const media1 = {
+//   game: { 
+//     title: "Final Fantasy IX",
+//     composer: "Nobuo Uematsu"
+//   }, 
+//   movie: {
+//     title: "Lord of the Rings",
+//     composer: "Howard Shore"
+//   }
+// };
 
-const media2 = {
-  game: { 
-    title: "Metal Gear Solid 2",
-    composer: "Harry Gregson-Williams"
-  }, 
-  movie: {
-    title: "Inception",
-    composer: "Hans Zimmer"
-  }
-};
+// const media2 = {
+//   game: { 
+//     title: "Metal Gear Solid 2",
+//     composer: "Harry Gregson-Williams"
+//   }, 
+//   movie: {
+//     title: "Inception",
+//     composer: "Hans Zimmer"
+//   }
+// };
 
-const media3 = {
-  game: { 
-    title: "Final Fantasy IX",
-    composer: "Nobuo Uematsu"
-  }, 
-  movie: {
-    title: "Lord of the Rings",
-    composer: "Howard Shore"
-  }
-};
+// const media3 = {
+//   game: { 
+//     title: "Final Fantasy IX",
+//     composer: "Nobuo Uematsu"
+//   }, 
+//   movie: {
+//     title: "Lord of the Rings",
+//     composer: "Howard Shore"
+//   }
+// };
 
-assertObjectsEqual(media1, media2); //false
-assertObjectsEqual(media1, media3); //true
+// assertObjectsEqual(media1, media2); //false
+// assertObjectsEqual(media1, media3); //true
