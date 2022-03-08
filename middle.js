ffdf35d5226951e3ece721fab42aae0b02e8c669
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-08 19:58:12 +0000

    middle.js

---
+++ middle.js
@@ -0,0 +1,40 @@
+const assertArraysEqual = function(actual, expected) {
+  let result = "";
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const middle = function(array) {
+  let len = array.length;
+  if (len <= 2) {
+    return [];
+  }
+  if (len % 2 === 0) { // if even
+    let mid = len / 2;
+    return [array[mid - 1],array[mid]];
+  } 
+  return [array[Math.floor(len/2)]];
+};
+
+
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
