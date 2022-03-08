527d034595b8496b640842f521759bf3a7cb340e
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-08 22:22:21 +0000

    findkeybyvalue

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  let result = "";
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(toSearch, target) {
+  for (let searchee in toSearch) {
+    if (toSearch[searchee] === target) return searchee;
+  }
+  return undefined;
+};
+
+// Test Cases
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const bestGamesBySeries = { 
+  final_fantasy: "Final Fantasy IX",
+  dragon_quest: "Dragon Quest IX",
+  halo: "Halo Infinite",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestGamesBySeries, "Final Fantasy IX"), "final_fantasy");
+assertEqual(findKeyByValue(bestGamesBySeries, "Cyberpunk"), undefined);
