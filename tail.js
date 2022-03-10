53d6154d1babc3fce53f29ef4e410d43ed0eca5c
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-10 19:43:05 +0000

    test package.json module and import/export

--- tail.js
+++ tail.js
@@ -1,10 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+import { assertEqual } from "./assertEqual.mjs";
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 const tail = function(array) {
   return array.slice(1,);
