a31f7594e99e0a59fa9d03e6ffc49c8330766fb8
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-10 18:18:51 +0000

    takeuntil

---
+++ takeUntil.js
@@ -0,0 +1,23 @@
+const takeUntil = function (a, c) {
+  let r = [];
+  for (let e of a) {
+    if (!c(e)) {
+      r.push(e);
+    } else {
+      break;
+    }
+  }
+  return r;
+};
+
+
+// Test Cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
