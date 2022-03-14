91915aae55886e87d6e35ec017f6c2058956e283
Nathan T <42843617+nathan-ts@users.noreply.github.com>
2022-03-14 19:42:11 +0000

    refactor tests

--- test/assertEqualTest.js
+++
@@ -1,7 +0,0 @@
-const assertEqual = require('../assertEqual');
-
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(1,2);
-// assertEqual("hello","hello");
